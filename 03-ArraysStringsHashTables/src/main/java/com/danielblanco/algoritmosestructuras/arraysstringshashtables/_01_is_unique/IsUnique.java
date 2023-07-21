d1c0abedcbffb35fa7ef9a98ffe87abdbb1ca6d8
DiegoR <diegorsdiaz@gmail.com>
2023-07-20 21:34:35 -0500

    solucion primeros dos ejercicios

--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
@@ -5,10 +5,32 @@ package com.danielblanco.algoritmosestructuras.arraysstringshashtables._01_is_un
  *
  * isUnique("abcde") => true;
  * isUnique("abcded") => false;
+ * a: b c d e 
+ * b: c d e
+ * c: d e
+ * 
+ * O(n^2)
+ * 
+ * Tabla Hash
+ * a b c d e
+ * 
+ * for O(N)
+ * Consulta en la tabla O(1)
+ * 
+ * -----
+ * O(1)
  */
 public class IsUnique {
 
+  private static NUMBER_OF_CHARS = 128;
   public boolean isUnique(String s) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    if(s.length() > NUMBER_OF_CHARS) return false;
+    
+    Set<Character> characters = new HashSet <> ();
+    for(Character c : s.toCharArray()) {
+      if(characters.contains(c)) return false;
+      characters.add(c);
+    }
+    return true;
   }
 }
